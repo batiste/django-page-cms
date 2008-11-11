57170a9587b1832c2c08bc6e5ba02f4b57b027e2
Batiste Bieler <batiste.bieler@gmail.com>
2008-11-11 14:44:32 +0000

    Fix a minor bug. Need testing
    git-svn-id: http://django-page-cms.googlecode.com/svn/trunk@193 439a9e5f-3f3e-0410-bc46-71226ad0111b

--- pages/settings.py
+++ pages/settings.py
@@ -22,7 +22,7 @@ PAGE_PERMISSION = getattr(settings, 'PAGE_PERMISSION', True)
 PAGE_TAGGING = getattr(settings, 'PAGE_TAGGING', True)
 
 # Whether to only allow unique slugs.
-PAGE_UNIQUE_SLUG_REQUIRED = getattr(settings, 'PAGE_UNIQUE_SLUG_REQUIRED', False)
+PAGE_UNIQUE_SLUG_REQUIRED = getattr(settings, 'PAGE_UNIQUE_SLUG_REQUIRED', True)
 
 # Whether to enable revisions.
 PAGE_CONTENT_REVISION = getattr(settings, 'PAGE_CONTENT_REVISION', True)
@@ -30,16 +30,17 @@ PAGE_CONTENT_REVISION = getattr(settings, 'PAGE_CONTENT_REVISION', True)
 # Defines which languages should be offered.
 PAGE_LANGUAGES = getattr(settings, 'PAGE_LANGUAGES', settings.LANGUAGES)
 
-# Defines how long page content should be cached, including navigation.
+# Defines how long page content should be cached, including navigation and admin menu.
 PAGE_CONTENT_CACHE_DURATION = getattr(settings, 'PAGE_CONTENT_CACHE_DURATION', 60)
 
 # The id of default Site instance to be used for multisite purposes.
 SITE_ID = getattr(settings, 'SITE_ID', 1)
 
-# You can exclude some placeholder from revision process
+# You can exclude some placeholder from the revision process
 PAGE_CONTENT_REVISION_EXCLUDE_LIST = getattr(settings, 'PAGE_CONTENT_REVISION_EXCLUDE_LIST', ())
 
 # Sanitize the user input with html5lib
 PAGE_SANITIZE_USER_INPUT = getattr(settings, 'PAGE_SANITIZE_USER_INPUT', False)
 
+# TODO: Make a description for this parameter
 PAGES_MEDIA_URL = getattr(settings, 'PAGES_MEDIA_URL', join(settings.MEDIA_URL, 'pages/'))

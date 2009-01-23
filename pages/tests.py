2fc13f58d8deba0f34c1e33ed78884630e8a7804
Batiste Bieler <batiste.bieler@gmail.com>
2009-01-23 12:00:51 +0000

    Add a create page test
    git-svn-id: http://django-page-cms.googlecode.com/svn/trunk@300 439a9e5f-3f3e-0410-bc46-71226ad0111b

--- pages/tests.py
+++ pages/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+
 from pages.models import *
 from django.test.client import Client
 
@@ -7,10 +8,20 @@ class PagesTestCase(TestCase):
 
     def test_01_add_page(self):
         """
-        Test that the add admin page could be displayed correctly
+        Test that the add admin page could be displayed via the admin
         """
         c = Client()
         c.login(username= 'batiste', password='b')
         response = c.get('/admin/pages/page/add/')
         assert(response.status_code == 200)
-        
+
+
+    def test_02_create_page(self):
+        """
+        Test that a page can be created via the admin
+        """
+        c = Client()
+        c.login(username= 'batiste', password='b')
+        page_data = {'title':'test page', 'slug':'test-page', 'language':'en', 'sites':[1], 'status':1}
+        response = c.post('/admin/pages/page/add/', page_data)
+        self.assertRedirects(response, '/admin/pages/page/')

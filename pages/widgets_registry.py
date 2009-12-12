f4ccb7d829a75f9b7be0947c6e56ecc743cb6525
Batiste Bieler <batisteb@opera.com>
2009-12-12 13:56:45 +0100

    Forget the widget registery module

---
+++ pages/widgets_registry.py
@@ -0,0 +1,31 @@
+__all__ = ('register_widget',)
+from django.utils.translation import ugettext as _
+
+class WidgetAlreadyRegistered(Exception):
+    """
+    An attempt was made to register a widget for Django page CMS more than once.
+    """
+    pass
+
+class WidgetNotFound(Exception):
+    """
+    The requested widget was not found
+    """
+    pass
+
+registry = []
+
+def register_widget(widget):
+
+    if widget in registry:
+        raise AlreadyRegistered(
+            _('The widget %s has already been registered.') % widget.__name__)
+    registry.append(widget)
+
+def get_widget(name):
+
+    for widget in registry:
+        if widget.__name__ == name:
+            return widget
+    raise WidgetNotFound(
+        _('The widget %s has not been registered.') % name)

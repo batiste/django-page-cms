04d2f8af6f9a20df7e6c72da43692828da3f87c6
Batiste Bieler <batisteb@opera.com>
2009-12-17 15:09:57 +0100

    Remove url to the document application of the package

--- example/urls.py
+++ example/urls.py
@@ -1,14 +1,32 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import url, include, patterns
+from django.conf.urls.defaults import handler404, handler500
 from django.contrib import admin
 from django.conf import settings
+from pages import settings as pages_settings
+from pages.views import details
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^i18n/', include('django.conf.urls.i18n')),
-    (r'^pages/', include('pages.urls')),
+    # We redefine urls down there to be able to use the document application
+    #(r'^pages/', include('pages.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
+
+if pages_settings.PAGE_USE_LANGUAGE_PREFIX:
+    urlpatterns += patterns('',
+        url(r'^pages/(?P<lang>[-\w]+)/(?P<path>.*)/documents/',
+            include("example.documents.urls")),
+        url(r'^pages/(?P<lang>[-\w]+)/(?P<path>.*)$', details,
+            name='pages-details-by-path')
+    )
+else:
+    urlpatterns += patterns('',
+        url(r'^pages/$', details, name='pages-root'),
+        url(r'^pages/(?P<path>.*)/documents/', include("example.documents.urls")),
+        url(r'^pages/(?P<path>.*)$', details, name='pages-details-by-path')
+    )
 
 if settings.DEBUG:
     urlpatterns += patterns('',
--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import url, include, patterns
+from django.conf.urls.defaults import handler404, handler500
 from pages.views import details
 from pages import settings
 
@@ -10,11 +10,9 @@ urlpatterns = patterns('',
 
 if settings.PAGE_USE_LANGUAGE_PREFIX:
     urlpatterns += patterns('',
-        url(r'^(?P<lang>[-\w]+)/(?P<path>.*)/documents/', include("example.documents.urls")),
         url(r'^(?P<lang>[-\w]+)/(?P<path>.*)$', details, name='pages-details-by-path')
     )
 else:
     urlpatterns += patterns('',
-        url(r'^(?P<path>.*)/documents/', include("example.documents.urls")),
         url(r'^(?P<path>.*)$', details, name='pages-details-by-path')
     )

aeea006abdd2a7235c2eb6ba1ac8c9e3a74ea602
Batiste Bieler <batisteb@opera.com>
2010-01-25 21:30:45 +0800

    Fix some bugs in the model, fix delegation example to run

--- example/documents/urls.py
+++ example/documents/urls.py
@@ -4,6 +4,6 @@ from example.documents.views import document_view
 from pages.http import pages_view
 
 urlpatterns = patterns('',
+    url(r'(?P<document_id>[0-9]+)$', pages_view(document_view), name='document_details'),
     url(r'$', pages_view(document_view), name='document_root'),
-    url(r'(?P<document_id>.*)$', pages_view(document_view), name='document_details')
 )
--- example/documents/views.py
+++ example/documents/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render_to_response
 from django.template import loader, Context, RequestContext
 from example.documents.models import Document
 
-def document_view(request, **kwargs):
+def document_view(request, *args, **kwargs):
     context = RequestContext(request, kwargs)
-    documents = Document.objects.filter(page=kwargs['current_page'])
-    context['documents'] = documents
+    if kwargs.get('current_page', False):
+        documents = Document.objects.filter(page=kwargs['current_page'])
+        context['documents'] = documents
     if kwargs.has_key('document_id'):
         document = Document.objects.get(pk=int(kwargs['document_id']))
         context['document'] = document

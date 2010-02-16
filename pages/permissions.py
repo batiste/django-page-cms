af8d14333aae4c1ff73c7f9dfd9bc3919d53c8c3
Batiste Bieler <batisteb@opera.com>
2010-02-16 14:20:57 +0100

    A bit of refactoring

--- pages/permissions.py
+++ pages/permissions.py
@@ -22,34 +22,7 @@ class PagePermission(authority.permissions.BasePermission):
             return True
         
         if action == 'change':
-            # the user has always the right to look at a page content
-            # if he doesn't try to modify it
-            if method == 'POST':
-                # right to change all the pages
-                if self.change_page():
-                    return True
-                if lang:
-                    # try the global language permission first
-                    perm = self.user.has_perm(
-                        'pages.can_manage_%s' % lang.replace('-', '_')
-                    )
-                    if perm:
-                        return True
-                    # then per object permission
-                    perm_func = getattr(self, 'manage (%s)_page' % lang)
-                    if perm_func(page):
-                        return True
-                # hierarchic permissions
-                perm_func = getattr(self, 'manage hierarchy_page')
-                if perm_func(page):
-                    return True
-                else:
-                    for ancestor in page.get_ancestors():
-                        if perm_func(ancestor):
-                            return True
-                return False
-            else:
-                return True
+            return self.has_change_permission(page, lang, method)
         if action == 'delete':
             if not self.delete_page():
                 return False
@@ -63,6 +36,41 @@ class PagePermission(authority.permissions.BasePermission):
             if perm:
                 return True
             return False
+        return False
+
+    def has_change_permission(self, page, lang, method=None):
+        """Return ``True`` if the current user has permission to
+        change the page."""
+        
+        # the user has always the right to look at a page content
+        # if he doesn't try to modify it.
+        if method != 'POST':
+            return True
+            
+        # right to change all the pages
+        if self.change_page():
+            return True
+        if lang:
+            # try the global language permission first
+            perm = self.user.has_perm(
+                'pages.can_manage_%s' % lang.replace('-', '_')
+            )
+            if perm:
+                return True
+            # then per object permission
+            perm_func = getattr(self, 'manage (%s)_page' % lang)
+            if perm_func(page):
+                return True
+        # last hierarchic permissions because it's more expensive
+        perm_func = getattr(self, 'manage hierarchy_page')
+        if perm_func(page):
+            return True
+        else:
+            for ancestor in page.get_ancestors():
+                if perm_func(ancestor):
+                    return True
+
+        # everything else failed, no permissions
         return False
 
 

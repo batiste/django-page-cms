9e8c800a977ff93dfcf8c8fda470acf8d25e6dac
Batiste Bieler <batisteb@opera.com>
2010-02-25 15:08:34 +0100

    Split unit/functionnal tests.

--- pages/tests/__init__.py
+++ pages/tests/__init__.py
@@ -1,5 +1,6 @@
 """Django page CMS test suite module"""
 from pages.tests.pages_tests import PagesTestCase
+from pages.tests.functionnal_tests import FunctionnalTestCase
 from pages.tests.auto_render import AutoRenderTestCase
 from pages.tests.regression_tests import RegressionTestCase
 from pages.tests.pages_link_tests import LinkTestCase

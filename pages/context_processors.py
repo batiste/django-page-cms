b3cdb6977948bccc0910b1de8105289efa1afbbe
Ian Ward <ian@excess.org>
2010-03-10 18:55:18 -0500

    PAGE_HIDE_SITES setting: use w/PAGE_USE_SITE_ID=True to hide the sites setting and only show pages from the current site used to access the admin, allows administration of separate page-cms sites with the same DB

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -5,7 +5,8 @@ def media(request):
     """Adds media-related variables to the `context`."""
     return {
         'PAGES_MEDIA_URL': settings.PAGES_MEDIA_URL,
-        'PAGE_USE_SITE_ID': settings.PAGE_USE_SITE_ID
+        'PAGE_USE_SITE_ID': settings.PAGE_USE_SITE_ID,
+        'PAGE_HIDE_SITES': settings.PAGE_HIDE_SITES,
     }
 
 def pages_navigation(request):

04028f5ac40f1ad79d395eac8e07bfe47b4e1b62
Ian Ward <ian@excess.org>
2010-03-12 22:39:42 +0800

    use order_by instead of Max in language_content_up_to_date templatetag

--- pages/templatetags/pages_tags.py
+++ pages/templatetags/pages_tags.py
@@ -439,12 +439,12 @@ def language_content_up_to_date(page, language):
         return True
     # get the last modified date for the official version
     last_modified = Content.objects.filter(language=lang_code,
-        page=page).aggregate(Max('creation_date'))['creation_date__max']
-    if last_modified is None:
+        page=page).order_by('-creation_date')
+    if not last_modified:
         # no official version
         return True
     lang_modified = Content.objects.filter(language=language,
-        page=page).aggregate(Max('creation_date'))['creation_date__max']
-    return lang_modified > last_modified
+        page=page).order_by('-creation_date')[0].creation_date
+    return lang_modified > last_modified[0].creation_date
 register.filter(language_content_up_to_date)
 

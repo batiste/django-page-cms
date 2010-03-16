0f1ac577e8d2cc86f14ea08c24128220f1a6b534
Batiste Bieler <batisteb@opera.com>
2010-03-16 13:13:52 +0100

    Move test templates and document within the package.

---
+++ pages/tests/documents/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+from pages.tests.documents.views import document_view
+from pages.http import pages_view
+
+urlpatterns = patterns('',
+    url(r'(?P<document_id>[0-9]+)$', pages_view(document_view), name='document_details'),
+    url(r'$', pages_view(document_view), name='document_root'),
+)

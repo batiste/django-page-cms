ab88748dcd5f48dce753655e0e0802a8b4d7d8b8
Steve Losh <steve@dwaiter.com>
2010-03-24 22:07:00 +0800

    Add a url attribute to the SearchIndex for pages.
    This is useful when displaying a list of search results because we
    can create a link to the result without having to hit the database
    for every object in the result list.

--- pages/search_indexes.py
+++ pages/search_indexes.py
@@ -9,6 +9,7 @@ class PageIndex(SearchIndex):
     """Search index for pages content."""
     text = CharField(document=True, use_template=True)
     title = CharField(model_attr='title')
+    url = CharField(model_attr='get_absolute_url')
     publication_date = DateTimeField(model_attr='publication_date')
 
     def get_queryset(self):

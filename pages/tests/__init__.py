7665e2b0af042948dfc7a1814275cd3309f5f6cf
Batiste Bieler <batisteb@opera.com>
2010-09-14 11:27:25 +0200

    Remove the dependency to django-unittest-depth

--- pages/tests/__init__.py
+++ pages/tests/__init__.py
@@ -1,5 +1,16 @@
 """Django page CMS test suite module"""
-from djangox.test.depth import alltests
+import unittest
 
 def suite():
-    return alltests(__file__, __name__)
+    suite = unittest.TestSuite()
+    from pages.tests.test_functionnal import FunctionnalTestCase
+    from pages.tests.test_unit import UnitTestCase
+    from pages.tests.test_regression import RegressionTestCase
+    from pages.tests.test_pages_link import LinkTestCase
+    from pages.tests.test_auto_render import AutoRenderTestCase
+    suite.addTest(unittest.makeSuite(FunctionnalTestCase))
+    suite.addTest(unittest.makeSuite(UnitTestCase))
+    suite.addTest(unittest.makeSuite(RegressionTestCase))
+    suite.addTest(unittest.makeSuite(LinkTestCase))
+    suite.addTest(unittest.makeSuite(AutoRenderTestCase))
+    return suite

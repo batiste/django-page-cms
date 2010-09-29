5b8e5d0aa796bcafdc8793c10b7b67979a4a5402
Batiste Bieler <batisteb@opera.com>
2010-09-29 10:55:58 +0200

    A bit more changes to make the default view even more flexible.

--- pages/views.py
+++ pages/views.py
@@ -11,35 +11,25 @@ from django.utils import translation
 
 class Details(object):
     """
-    This view get the root pages for navigation
+    This class based view get the root pages for navigation
     and the current page to display if there is any.
 
     All is rendered with the current page's template.
-
-    This view use the auto_render decorator. It means
-    that you can use the only_context extra parameter to get
-    only the local variables of this view without rendering
-    the template.
-
-    >>> from pages.views import details
-    >>> context = details(request, only_context=True)
-
-    This can be usefull if you want to write your own
-    view. You can reuse the following code without having to
-    copy and paste it."""
+    """
 
     def __call__(self, request, path=None, lang=None, delegation=True,
             **kwargs):
 
-        pages_navigation = self.get_navigation(request)
         current_page = False
-        lang = self.get_language(lang, request)
+        lang = self.choose_language(lang, request)
 
         # if the path is not defined, we assume that the user
         # is using the view in a non usual way and fallback onto the
         # the full request path.
         if path is None:
             path = request.path
+
+        pages_navigation = self.get_navigation(request, path, lang)
 
         context = {
             'path': path,
@@ -48,24 +38,61 @@ class Details(object):
         }
 
         is_staff = self.is_user_staff(request)
-        if path:
-            current_page = Page.objects.from_path(path, lang,
-                exclude_drafts=(not is_staff))
-        elif pages_navigation:
-            current_page = Page.objects.root()[0]
+
+        current_page = self.resolve_page(request, context, is_staff)
 
         # if no pages has been found, we will try to find it via an Alias
         if not current_page:
-            alias = PageAlias.objects.from_path(request, path, lang)
-            if alias:
-                url = alias.page.get_url_path(lang)
-                return HttpResponsePermanentRedirect(url)
-            raise Http404
+            redirection = self.resolve_alias(request, path, lang)
+            if redirection:
+                return redirection
+        else:
+            context['current_page'] = current_page
 
-        # If unauthorized to see the pages, raise a 404
+        # If unauthorized to see the pages, raise a 404, That can
+        # happen with expired pages.
         if not is_staff and not current_page.visible:
             raise Http404
 
+        redirection = self.resolve_redirection(request, context)
+        if redirection:
+            return redirection
+
+        template_name = self.get_template(request, context)
+
+        if request.is_ajax():
+            template_name = "body_%s" % template_name
+
+        self.extra_context(request, context)
+
+        answer = self.delegate(request, context, delegation)
+        if answer:
+            return answer
+
+        return template_name, context
+
+    def resolve_page(self, request, context, is_staff):
+        """Return the appropriate page according to the path."""
+        path = context['path']
+        lang = context['lang']
+        pages_navigation = context['pages_navigation']
+        if path:
+            return Page.objects.from_path(path, lang,
+                exclude_drafts=(not is_staff))
+        elif pages_navigation:
+            return Page.objects.root()[0]
+
+    def resolve_alias(self, request, path, lang):
+        alias = PageAlias.objects.from_path(request, path, lang)
+        if alias:
+            url = alias.page.get_url_path(lang)
+            return HttpResponsePermanentRedirect(url)
+        raise Http404
+
+    def resolve_redirection(self, request, context):
+        """Check for redirections."""
+        current_page = context['current_page']
+        lang = context['lang']
         if current_page.redirect_to_url:
             return HttpResponsePermanentRedirect(current_page.redirect_to_url)
 
@@ -73,40 +100,11 @@ class Details(object):
             return HttpResponsePermanentRedirect(
                 current_page.redirect_to.get_url_path(lang))
 
-        template_name = current_page.get_template()
-
-        if request.is_ajax():
-            template_name = "body_%s" % template_name
-
-        if current_page:
-            context['current_page'] = current_page
-
-        self.extra_context(request, context)
-
-        # if there is a delegation to another view,
-        # call this view instead.
-        if delegation and current_page.delegate_to:
-            urlconf = get_urlconf(current_page.delegate_to)
-            result = resolve('/', urlconf)
-            if len(result):
-                view, args, kwargs = result
-                kwargs['current_page'] = current_page
-                kwargs['path'] = path
-                kwargs['lang'] = lang
-                kwargs['pages_navigation'] = pages_navigation
-                return view(
-                    request,
-                    *args,
-                    **kwargs
-                )
-
-        return template_name, context
-
-    def get_navigation(self, request):
+    def get_navigation(self, request, path, lang):
         """Get the pages that are at the root level."""
         return Page.objects.navigation().order_by("tree_id")
 
-    def get_language(self, lang, request):
+    def choose_language(self, lang, request):
         """Deal with the multiple corner case of choosing the language."""
 
         # Can be an empty string or None
@@ -124,6 +122,10 @@ class Details(object):
 
         return lang
 
+    def get_template(self, request, context):
+        """Just there in case you have special business logic."""
+        return context['current_page'].get_template()
+
     def is_user_staff(self, request):
         """Return True if the user is staff."""
         return request.user.is_authenticated() and request.user.is_staff
@@ -133,4 +135,24 @@ class Details(object):
         if settings.PAGE_EXTRA_CONTEXT:
             context.update(settings.PAGE_EXTRA_CONTEXT())
 
+    def delegate(self, request, context, delegation=True):
+        # if there is a delegation to another view,
+        # call this view instead.
+        current_page = context['current_page']
+        if delegation and current_page.delegate_to:
+            urlconf = get_urlconf(current_page.delegate_to)
+            result = resolve('/', urlconf)
+            if len(result):
+                view, args, kwargs = result
+                kwargs.update(context)
+                return view(request, *args, **kwargs)
+
+
+# This view instance use the auto_render decorator. It means
+# that you can use the only_context extra parameter to get
+# only the local variables of this view without rendering
+# the template.
+
+#   >>> from pages.views import details
+#   >>> context = details(request, only_context=True)
 details = auto_render(Details())

503d5b4f47f558d6415c10b6cfeb03e31cc1ed6c
Dan Hilton <dan.hilton@wearefoundry.com>
2011-01-10 20:08:21 +0000

    Added setting for allowing realtime search index rather than index on management command

--- pages/search_indexes.py
+++ pages/search_indexes.py
@@ -1,7 +1,8 @@
 """Django haystack `SearchIndex` module."""
 from pages.models import Page
+from django.conf import settings
 
-from haystack.indexes import SearchIndex, CharField, DateTimeField
+from haystack.indexes import SearchIndex, CharField, DateTimeField, RealTimeSearchIndex
 from haystack import site
 
 
@@ -16,4 +17,20 @@ class PageIndex(SearchIndex):
         """Used when the entire index for model is updated."""
         return Page.objects.published()
 
-site.register(Page, PageIndex)
+
+class RealTimePageIndex(RealTimeSearchIndex):
+    """Search index for pages content."""
+    text = CharField(document=True, use_template=True)
+    title = CharField(model_attr='title')
+    url = CharField(model_attr='get_absolute_url')
+    publication_date = DateTimeField(model_attr='publication_date')
+
+    def get_queryset(self):
+        """Used when the entire index for model is updated."""
+        return Page.objects.published()
+
+if settings.PAGE_REAL_TIME_SEARCH:
+    site.register(Page, RealTimePageIndex)
+else:
+    site.register(Page, PageIndex)
+    

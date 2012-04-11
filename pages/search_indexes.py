c0bee77fd594416a7ecbbba28b88046a54dc8730
Ian Ward <ian@excess.org>
2012-04-10 21:10:42 -0400

    haystack development version compat

--- pages/search_indexes.py
+++ pages/search_indexes.py
@@ -3,34 +3,34 @@ from pages.models import Page
 from pages import settings
 
 from haystack.indexes import SearchIndex, CharField, DateTimeField, RealTimeSearchIndex
-from haystack import site
-
-
-class PageIndex(SearchIndex):
-    """Search index for pages content."""
-    text = CharField(document=True, use_template=True)
-    title = CharField(model_attr='title')
-    url = CharField(model_attr='get_absolute_url')
-    publication_date = DateTimeField(model_attr='publication_date')
-
-    def get_queryset(self):
-        """Used when the entire index for model is updated."""
-        return Page.objects.published()
-
-
-class RealTimePageIndex(RealTimeSearchIndex):
-    """Search index for pages content."""
-    text = CharField(document=True, use_template=True)
-    title = CharField(model_attr='title')
-    url = CharField(model_attr='get_absolute_url')
-    publication_date = DateTimeField(model_attr='publication_date')
-
-    def get_queryset(self):
-        """Used when the entire index for model is updated."""
-        return Page.objects.published()
 
 if settings.PAGE_REAL_TIME_SEARCH:
-    site.register(Page, RealTimePageIndex)
+    class RealTimePageIndex(RealTimeSearchIndex):
+        """Search index for pages content."""
+        text = CharField(document=True, use_template=True)
+        title = CharField(model_attr='title')
+        url = CharField(model_attr='get_absolute_url')
+        publication_date = DateTimeField(model_attr='publication_date')
+
+        def get_queryset(self):
+            """Used when the entire index for model is updated."""
+            return Page.objects.published()
+
+        def get_model(self):
+            Page
+
 else:
-    site.register(Page, PageIndex)
+    class PageIndex(SearchIndex):
+        """Search index for pages content."""
+        text = CharField(document=True, use_template=True)
+        title = CharField(model_attr='title')
+        url = CharField(model_attr='get_absolute_url')
+        publication_date = DateTimeField(model_attr='publication_date')
+
+        def get_queryset(self):
+            """Used when the entire index for model is updated."""
+            return Page.objects.published()
+
+        def get_model(self):
+            Page
 

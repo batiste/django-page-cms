c3a61f190994de82b0909aa22613c02eeb5a2438
Batiste Bieler <batiste.bieler@gmail.com>
2012-05-30 16:53:08 +0200

    Fix all the tests

--- gerbi/tests/__init__.py
+++ gerbi/tests/__init__.py
@@ -11,6 +11,8 @@ def suite():
         return suite
     suite.addTest(unittest.makeSuite(UnitTestCase))
     suite.addTest(unittest.makeSuite(RegressionTestCase))
+    suite.addTest(unittest.makeSuite(LinkTestCase))
+    suite.addTest(unittest.makeSuite(AutoRenderTestCase))
     # being the slower test I run it at the end
     suite.addTest(unittest.makeSuite(FunctionnalTestCase))
     return suite

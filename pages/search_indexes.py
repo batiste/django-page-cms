11c81e7bdf783fa5cd08647fc0d5d5bae68e0fa5
Matt Miller <matt@flashnotes.com>
2013-05-24 18:55:40 -0400

    Haystack 2 updates
    No RealtimeSearchIndex use the SignalProcessor.

--- pages/search_indexes.py
+++ pages/search_indexes.py
@@ -1,16 +1,22 @@
+# -*- coding: utf-8 -*-
 """Django haystack `SearchIndex` module."""
 from pages.models import Page
 from pages import settings
 
-from haystack.indexes import SearchIndex, CharField, DateTimeField, RealTimeSearchIndex
+from haystack.indexes import SearchIndex, CharField, DateTimeField, Indexable
+
 
 if settings.PAGE_REAL_TIME_SEARCH:
-    class RealTimePageIndex(RealTimeSearchIndex):
+    class RealTimePageIndex(SearchIndex):
         """Search index for pages content."""
         text = CharField(document=True, use_template=True)
         title = CharField(model_attr='title')
         url = CharField(model_attr='get_absolute_url')
         publication_date = DateTimeField(model_attr='publication_date')
+
+        def index_queryset(self, using=None):
+            """ Haystack 2.0 requires this method now """
+            return self.get_model().objects.published()
 
         def get_queryset(self):
             """Used when the entire index for model is updated."""
@@ -20,7 +26,7 @@ if settings.PAGE_REAL_TIME_SEARCH:
             Page
 
 else:
-    class PageIndex(SearchIndex):
+    class PageIndex(SearchIndex, Indexable):
         """Search index for pages content."""
         text = CharField(document=True, use_template=True)
         title = CharField(model_attr='title')

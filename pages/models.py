77590ce659edca4538e671f1618d9bf3a9c7a9db
Markus Törnqvist <mjt+illyria@skyhood.com>
2013-08-21 17:12:32 +0300

    Getting content is real heavy, cache it

--- pages/models.py
+++ pages/models.py
@@ -367,8 +367,13 @@ class Page(MPTTModel):
         :param fallback: if ``True``, the content will also be searched in \
         other languages.
         """
-        return Content.objects.get_content(self, language, ctype,
-            language_fallback)
+        key = 'content_%d_%s_%s_%s' % (self.id, language, ctype, language_fallback)
+        content = cache.get(key, None)
+        if content is None:
+            content = Content.objects.get_content(self, language, ctype,
+                language_fallback)
+            cache.set(key, content)
+        return content
 
     def expose_content(self):
         """Return all the current content of this page into a `string`.

3823adce83bf45d8966b22201f422f6ad3f73dd4
Batiste Bieler <batiste.bieler@gmail.com>
2013-10-29 12:04:15 +0100

    Commit the jsonexport plugin files

---
+++ pages/plugins/jsonexport/__init__.py
@@ -0,0 +1,5 @@
+PAGE_PLUGIN = True
+
+#PAGE_ADMIN_URLS = "pages.plugins.jsonexport.admin_urls"
+PAGE_ADMIN_ACTIONS = ["pages.plugins.jsonexport.actions.export_pages_as_json", 
+    "pages.plugins.jsonexport.actions.import_pages_from_json"]

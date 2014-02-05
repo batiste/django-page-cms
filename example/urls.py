9c971155d73125f4b997cc1ed1619be71ea69b68
Batiste Bieler <batiste.bieler@local.ch>
2014-02-05 13:04:48 +0100

    Add tests for cinghiale fixes

--- example/urls.py
+++ example/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls import handler404, handler500
 from django.contrib import admin
 from pages.views import details
 
-from pages.urlconf_registry import register_urlconf
-register_urlconf('test', 'pages.testsproject.documents.urls')
+#from pages.urlconf_registry import register_urlconf
+#register_urlconf('test', 'pages.testsproject.documents.urls')
 
 admin.autodiscover()
 authority.autodiscover()
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     (r'^authority/', include('authority.urls')),
     (r'^i18n/', include('django.conf.urls.i18n')),
     (r'^admin/', include(admin.site.urls)),
-    (r'^grappelli/', include('grappelli.urls')),
+    #(r'^grappelli/', include('grappelli.urls')),
     # make tests fail if a backend is not present on the system
     url(r'^search/', include('haystack.urls'), name='haystack_search'),
 
